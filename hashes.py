79b9b8e523e4f94ef59baba0403f268eb2e6da6d
DamonCDB <101036086+DamonCDB@users.noreply.github.com>
2025-04-26 07:00:16 +0200

    Update hashes.py

--- hashes.py
+++ hashes.py
@@ -1,29 +1,14 @@
 import hashlib
 
-def to_md5():
-    file1 = input("Introduce la ruta del archivo del diccionario de contraseñas: ")
-    file2 = input("Introduce la ruta del archivo donde quieres guardar los hashes: ")
-    f = open(file1, "r", encoding='latin-1')
-    h = open(file2, "a")
+hash = input("Introduce el hash a buscar en el diccionario: ").strip()
+dict_path = input("Introduce la ruta del archivo del diccionario de contrasenyas: ").strip()
+f = open(dict_path, "r", encoding='latin-1')
+try:
     for line in f:
-        string_to_hash = line
-        hash_md5 = hashlib.md5(str(string_to_hash).encode('utf-8'))
-        h.write(hash_md5.hexdigest() + "\n")
-
-def check_md5(hash):
-    file1 = input("Introduce la ruta del archivo de los hashes del diccionario: ")
-    file2 = input("Introduce la ruta del archivo del diccionario de contraseñas: ")
-    h = open(file1, 'r', encoding='latin-1')
-    p = open(file2, 'r', encoding='latin-1')
-    lineas1 = h.readlines()
-    lineas2 = p.readlines()
-    i = 0
-    
-    for line in lineas1:
-        if hash in line:
-            print("El hash " + hash + " pertenece al password " + lineas2[i])
-        else:
-            i = i + 1
-
-# to_md5()
-# check_md5('hash_a_buscar')
+        password = line.strip()
+        hash_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
+        if hash == hash_md5:
+            print("El hash " + hash + " pertenece al password " + password)
+            break
+except FileNotFoundError:
+    print("No se pudo abrir el archivo. Verifica la ruta.")
